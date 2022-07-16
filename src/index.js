f5b934d8e92f2af4c8b1eaaa2df4c582348a675e
pamphilemkp <pamphilemkp@gmail.com>
2022-07-16 22:12:41 +0300

    add am remove and edit implemented

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import './style.css';
-import { Task, lists } from './HandListStorage';
+import { Task, lists } from './HandListStorage.js';
 
 class Todo {
-
-    static displayStoarageTasks(){
-      let ArrTasks = lists.getTasks();
-      ArrTasks.forEach((task)=> Todo.addlist(task));
-    }
-
-
+  static displayStoarageTasks() {
+    const ArrTasks = lists.getTasks();
+    ArrTasks.forEach((task) => Todo.addlist(task));
+  }
 
   static addlist(tasks) {
     const task = document.querySelector('.tasks');
     const listitem = document.createElement('li');
     listitem.classList.add('list');
-    listitem.innerHTML += `<div class='item-label'>
+    listitem.innerHTML += `<div class='item-label' >
     <div class='item-descriptiont'>
-      <input type="checkbox" id="check" name='check'>
-      <label for='' class="label" contentEditable="true">${tasks.description}</label>
+      <input type="checkbox" name='check'>
+      <input type="text" id="task-${tasks.index}" class="inputTask" value="${tasks.description}" disabled= true >
       </div>
     <div class='item-content'>
-      <button type="submit"  class="edit">Edit</button>
+    <button type="submit" id="${tasks.index}" class="edit">Edit</button>
       <button type="submit" class="delete">Delete</button>
   </div>
   </div> `;
@@ -43,34 +40,23 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   // validation
+  // eslint-disable-next-line no-empty
   if (input.value.trim() === '') {
-    return null;
-      } else {
-    // create a task with constructor and display it
-    const tasks = new Task(input.value, false, id);
-    id++;
-    Todo.addlist(tasks);
-
-        // clear the input field
-        input.value = '';
-
-    // add to tasks to localStorage
-    lists.addTask(tasks);
-
-
-
-    // line through the item when clicked
-    const check = document.querySelector('#check');
-    if (check.checked) {
-      const label = document.querySelectorAll('.label');
-      label.forEach((e) => e.classList.toggle('line-through'));
-    }
   }
+  // create a task with constructor and display it
+  const tasks = new Task(input.value, false, id);
+  id += 1;
+  Todo.addlist(tasks);
+
+  // clear the input field
+  input.value = '';
+
+  // add to tasks to localStorage
+  lists.addTask(tasks);
 });
 
 // call the Storage when the DomLoad
 document.addEventListener('DomContentLoaded', Todo.displayStoarageTasks());
-
 
 // deleting the task element
 document.querySelector('.tasks').addEventListener('click', (e) => {
@@ -78,8 +64,22 @@ document.querySelector('.tasks').addEventListener('click', (e) => {
   Todo.deleteTask(el);
 
   // delete to localStorage
-  const index = el.parentElement.previousElementSibling.lastElementChild.innerText;
+  const index = el.parentElement.previousElementSibling.lastElementChild.value;
   lists.deleteTaks(el, index);
+  // edit tasks
+  const ids = `task-${el.id}`;
+  const editTask = document.getElementById(ids);
+  editTask.disabled = false;
+
+  document.addEventListener('keypress', (i) => {
+    const tasks = lists.getTasks();
+    tasks[el.id].description = i.target.value;
+
+    if (i.key === 'Enter') {
+      localStorage.setItem('task', JSON.stringify(tasks));
+      editTask.disabled = true;
+    }
+  });
 });
 
 // clear all elements
