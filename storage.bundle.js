e17ba7c8f7da01c797a581363e29ac0cf3177693
pamphilemkp <pamphilemkp@gmail.com>
2022-07-18 13:56:52 +0300

    add deploy page

---
+++ storage.bundle.js
@@ -0,0 +1,27 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkmy_webpack_biolerplate"] = self["webpackChunkmy_webpack_biolerplate"] || []).push([["storage"],{
+
+/***/ "./src/HandListStorage.js":
+/*!********************************!*\
+  !*** ./src/HandListStorage.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Task)\n/* harmony export */ });\n\r\n class Task {\r\n    constructor(description,completed,index){\r\n        this.description = description;\r\n        this.completed = false;\r\n        this.index = index\r\n    }\r\n  };\r\n\r\nclass lists {\r\n  static displaytask(){\r\n    \r\n  }\r\n\r\n    //store elements with localStorage\r\n    getTasks(){\r\n        \r\n    }\r\n\r\n}\n\n//# sourceURL=webpack://my-webpack-biolerplate/./src/HandListStorage.js?");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/HandListStorage.js"));
+/******/ }
+]);
